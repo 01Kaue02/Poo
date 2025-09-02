d8fffb4103ed631e14fd6e0a03d93929ff386433
Kaue Lima <kauelyma@gmail.com>
2025-09-01 22:28:26 -0300

    adicionando remover quantidade adicionando tudo isso

--- Program.cs
+++ Program.cs
@@ -2,6 +2,32 @@
 
 Pessoa p1 = new Pessoa();
 p1.Nome = "Kauê";
-p1.Idade = 21;
 p1.Sobrenome = "Silva";
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Jhessica";
+p2.Sobrenome = "Isabel";
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//p1.Apresentar();
